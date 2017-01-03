8e2a6779dbddc8436390f1d35b6b677a4826451b
diegocairone <diegocairone@gmail.com>
2017-01-03 11:52:29 -0300

    PaisEDM - Consultas

--- odataexample/src/main/java/com/cairone/odataexample/datasources/PaisDataSource.java
+++ odataexample/src/main/java/com/cairone/odataexample/datasources/PaisDataSource.java
@@ -1,14 +1,22 @@
 package com.cairone.odataexample.datasources;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 import com.cairone.odataexample.edm.resources.PaisEdm;
+import com.cairone.odataexample.entities.PaisEntity;
+import com.cairone.odataexample.repositories.PaisRepository;
+import com.cairone.odataexample.strategyBuilders.PaisesStrategyBuilder;
+import com.mysema.query.types.expr.BooleanExpression;
 import com.sdl.odata.api.ODataException;
 import com.sdl.odata.api.ODataSystemException;
 import com.sdl.odata.api.edm.model.EntityDataModel;
+import com.sdl.odata.api.edm.util.EdmUtil;
 import com.sdl.odata.api.parser.ODataUri;
 import com.sdl.odata.api.parser.TargetType;
 import com.sdl.odata.api.processor.datasource.DataSource;
@@ -24,6 +32,8 @@ import com.sdl.odata.api.service.ODataRequestContext;
 @Component
 public class PaisDataSource implements DataSourceProvider, DataSource  {
 
+	@Autowired private PaisRepository paisRepository = null;
+	
 	@Override
 	public Object create(ODataUri uri, Object entity, EntityDataModel entityDataModel) throws ODataException {
 		return null;
@@ -64,16 +74,49 @@ public class PaisDataSource implements DataSourceProvider, DataSource  {
 
 	@Override
 	public QueryOperationStrategy getStrategy(ODataRequestContext requestContext, QueryOperation operation, TargetType expectedODataEntityType) throws ODataException {
+
+		PaisesStrategyBuilder builder = new PaisesStrategyBuilder();
+		BooleanExpression expression = builder.buildCriteria(operation, requestContext);
+		List<Sort.Order> orderByList = builder.getOrderByList();
+		
+		int limit = builder.getLimit();
+        int skip = builder.getSkip();
+		List<String> propertyNames = builder.getPropertyNames();
+		
+		List<PaisEntity> paisEntities = (List<PaisEntity>) ( orderByList == null || orderByList.size() == 0 ?
+				paisRepository.findAll(expression) :
+				paisRepository.findAll(expression, new Sort(orderByList)) );
 		
 		return () -> {
-			
-			List<PaisEdm> paisEdms = Arrays.asList(
-					new PaisEdm(1, "ARGENTINA"),
-					new PaisEdm(2, "URUGUAY")
-				);
 
-            QueryResult result = QueryResult.from(paisEdms);
+			List<PaisEdm> filtered = paisEntities.stream().map(entity -> { return new PaisEdm(entity); }).collect(Collectors.toList());
+			
+			long count = 0;
+        	
+            if (builder.isCount() || builder.includeCount()) {
+                count = filtered.size();
+
+                if (builder.isCount()) {
+                    return QueryResult.from(count);
+                }
+            }
+
+            if (skip != 0 || limit != Integer.MAX_VALUE) {
+                filtered = filtered.stream().skip(skip).limit(limit).collect(Collectors.toList());
+            }
+
+            if (propertyNames != null && !propertyNames.isEmpty()) {
+                try {
+                    return QueryResult.from(EdmUtil.getEdmPropertyValue(filtered.get(0), propertyNames.get(0)));
+                } catch (IllegalAccessException e) {
+                    return QueryResult.from(Collections.emptyList());
+                }
+            }
             
+            QueryResult result = QueryResult.from(filtered);
+            if (builder.includeCount()) {
+                result = result.withCount(count);
+            }
             return result;
 		};
 	}
