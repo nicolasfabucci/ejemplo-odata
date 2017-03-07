abbd100e8bf2220f3dc753420122605abfb68459
diegocairone <diegocairone@gmail.com>
2017-03-07 12:44:46 -0300

    Nuevo ProvinciasStrategyBuilder y actualizacion de ProvinciaDataSource

--- odataexample/src/main/java/com/cairone/odataexample/datasources/ProvinciaDataSource.java
+++ odataexample/src/main/java/com/cairone/odataexample/datasources/ProvinciaDataSource.java
@@ -1,9 +1,15 @@
 package com.cairone.odataexample.datasources;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.DataBinder;
@@ -17,9 +23,12 @@ import com.cairone.odataexample.entities.ProvinciaEntity;
 import com.cairone.odataexample.entities.ProvinciaPKEntity;
 import com.cairone.odataexample.repositories.PaisRepository;
 import com.cairone.odataexample.repositories.ProvinciaRepository;
+import com.cairone.odataexample.strategyBuilders.ProvinciasStrategyBuilder;
+import com.mysema.query.types.expr.BooleanExpression;
 import com.sdl.odata.api.ODataException;
 import com.sdl.odata.api.ODataSystemException;
 import com.sdl.odata.api.edm.model.EntityDataModel;
+import com.sdl.odata.api.edm.util.EdmUtil;
 import com.sdl.odata.api.parser.ODataUri;
 import com.sdl.odata.api.parser.ODataUriUtil;
 import com.sdl.odata.api.parser.TargetType;
@@ -29,6 +38,7 @@ import com.sdl.odata.api.processor.datasource.ODataDataSourceException;
 import com.sdl.odata.api.processor.datasource.TransactionalDataSource;
 import com.sdl.odata.api.processor.link.ODataLink;
 import com.sdl.odata.api.processor.query.QueryOperation;
+import com.sdl.odata.api.processor.query.QueryResult;
 import com.sdl.odata.api.processor.query.strategy.QueryOperationStrategy;
 import com.sdl.odata.api.service.ODataRequestContext;
 
@@ -186,8 +196,52 @@ public class ProvinciaDataSource implements DataSourceProvider, DataSource {
 
 	@Override
 	public QueryOperationStrategy getStrategy(ODataRequestContext requestContext, QueryOperation operation, TargetType expectedODataEntityType) throws ODataException {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
+		ProvinciasStrategyBuilder builder = new ProvinciasStrategyBuilder();
+		BooleanExpression expression = builder.buildCriteria(operation, requestContext);
+		List<Sort.Order> orderByList = builder.getOrderByList();
+		
+		int limit = builder.getLimit();
+        int skip = builder.getSkip();
+		List<String> propertyNames = builder.getPropertyNames();
+		
+		Page<ProvinciaEntity> pageProvinciaEntity = orderByList == null || orderByList.size() == 0 ?
+				provinciaRepository.findAll(expression, new PageRequest(0, limit)) :
+				provinciaRepository.findAll(expression, new PageRequest(0, limit, new Sort(orderByList)));
+		
+		List<ProvinciaEntity> provinciaEntities = pageProvinciaEntity.getContent();
+		
+		return () -> {
+
+			List<ProvinciaEdm> filtered = provinciaEntities.stream().map(entity -> { return new ProvinciaEdm(entity); }).collect(Collectors.toList());
+			
+			long count = 0;
+        	
+            if (builder.isCount() || builder.includeCount()) {
+                count = filtered.size();
+
+                if (builder.isCount()) {
+                    return QueryResult.from(count);
+                }
+            }
+
+            if (skip != 0 || limit != Integer.MAX_VALUE) {
+                filtered = filtered.stream().skip(skip).collect(Collectors.toList());
+            }
+
+            if (propertyNames != null && !propertyNames.isEmpty()) {
+                try {
+                    return QueryResult.from(EdmUtil.getEdmPropertyValue(filtered.get(0), propertyNames.get(0)));
+                } catch (IllegalAccessException e) {
+                    return QueryResult.from(Collections.emptyList());
+                }
+            }
+            
+            QueryResult result = QueryResult.from(filtered);
+            if (builder.includeCount()) {
+                result = result.withCount(count);
+            }
+            return result;
+		};
+	}
 }
