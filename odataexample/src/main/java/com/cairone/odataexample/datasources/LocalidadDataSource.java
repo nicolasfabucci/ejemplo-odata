f85cbf3e8b8f7713eccd7b1b2b512c0726879ca3
diegocairone <diegocairone@gmail.com>
2017-03-14 11:34:10 -0300

    Soporte para Localidades

---
+++ odataexample/src/main/java/com/cairone/odataexample/datasources/LocalidadDataSource.java
@@ -0,0 +1,203 @@
+package com.cairone.odataexample.datasources;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+import scala.Option;
+
+import com.cairone.odataexample.dtos.LocalidadFrmDto;
+import com.cairone.odataexample.dtos.validators.LocalidadFrmDtoValidator;
+import com.cairone.odataexample.edm.resources.LocalidadEdm;
+import com.cairone.odataexample.entities.LocalidadEntity;
+import com.cairone.odataexample.services.LocalidadService;
+import com.cairone.odataexample.strategyBuilders.LocalidadesStrategyBuilder;
+import com.cairone.odataexample.utils.SQLExceptionParser;
+import com.cairone.odataexample.utils.ValidatorUtil;
+import com.mysema.query.types.expr.BooleanExpression;
+import com.sdl.odata.api.ODataException;
+import com.sdl.odata.api.ODataSystemException;
+import com.sdl.odata.api.edm.model.EntityDataModel;
+import com.sdl.odata.api.edm.util.EdmUtil;
+import com.sdl.odata.api.parser.ODataUri;
+import com.sdl.odata.api.parser.ODataUriUtil;
+import com.sdl.odata.api.parser.TargetType;
+import com.sdl.odata.api.processor.datasource.DataSource;
+import com.sdl.odata.api.processor.datasource.DataSourceProvider;
+import com.sdl.odata.api.processor.datasource.ODataDataSourceException;
+import com.sdl.odata.api.processor.datasource.TransactionalDataSource;
+import com.sdl.odata.api.processor.link.ODataLink;
+import com.sdl.odata.api.processor.query.QueryOperation;
+import com.sdl.odata.api.processor.query.QueryResult;
+import com.sdl.odata.api.processor.query.strategy.QueryOperationStrategy;
+import com.sdl.odata.api.service.ODataRequestContext;
+
+@Component
+public class LocalidadDataSource implements DataSourceProvider, DataSource {
+
+	@Autowired private LocalidadService localidadService = null;
+	@Autowired private LocalidadFrmDtoValidator localidadFrmDtoValidator = null;
+
+	@Autowired
+	private MessageSource messageSource = null;
+
+	@Override
+	public Object create(ODataUri uri, Object entity, EntityDataModel entityDataModel) throws ODataException {
+
+		if(entity instanceof LocalidadEdm) {
+			
+			LocalidadEdm localidadEdm = (LocalidadEdm) entity;
+			LocalidadFrmDto localidadFrmDto = new LocalidadFrmDto(localidadEdm);
+
+			ValidatorUtil.validate(localidadFrmDtoValidator, messageSource, localidadFrmDto);
+
+			try {
+				LocalidadEntity localidadEntity = localidadService.nuevo(localidadFrmDto);
+				return new LocalidadEdm(localidadEntity);
+			} catch (Exception e) {
+				String message = SQLExceptionParser.parse(e);
+				throw new ODataDataSourceException(message);
+			}
+		}
+		
+		throw new ODataDataSourceException("LOS DATOS NO CORRESPONDEN A LA ENTIDAD LOCALIDAD");
+	}
+
+	@Override
+	public Object update(ODataUri uri, Object entity, EntityDataModel entityDataModel) throws ODataException {
+
+		if(entity instanceof LocalidadEdm) {
+
+    		Map<String, Object> oDataUriKeyValues = ODataUriUtil.asJavaMap(ODataUriUtil.getEntityKeyMap(uri, entityDataModel));
+    					
+			LocalidadEdm localidadEdm = (LocalidadEdm) entity;
+			LocalidadFrmDto localidadFrmDto = new LocalidadFrmDto(localidadEdm);
+
+			ValidatorUtil.validate(localidadFrmDtoValidator, messageSource, localidadFrmDto);
+
+			Integer localidadID = Integer.valueOf(oDataUriKeyValues.get("localidadId").toString());
+			Integer provinciaID = Integer.valueOf(oDataUriKeyValues.get("provinciaId").toString());
+			Integer paisID = Integer.valueOf(oDataUriKeyValues.get("paisId").toString());
+			
+			localidadFrmDto.setPaisId(paisID);
+			localidadFrmDto.setProvinciaId(provinciaID);
+			localidadFrmDto.setLocalidadId(localidadID);
+
+			try {
+				LocalidadEntity localidadEntity = localidadService.nuevo(localidadFrmDto);
+				return new LocalidadEdm(localidadEntity);
+			} catch (Exception e) {
+				String message = SQLExceptionParser.parse(e);
+				throw new ODataDataSourceException(message);
+			}
+		}
+		
+		throw new ODataDataSourceException("LOS DATOS NO CORRESPONDEN A LA ENTIDAD LOCALIDAD");
+	}
+
+	@Override
+	public void delete(ODataUri uri, EntityDataModel entityDataModel) throws ODataException {
+
+		Option<Object> entity = ODataUriUtil.extractEntityWithKeys(uri, entityDataModel);
+    	
+    	if(entity.isDefined()) {
+    		
+    		LocalidadEdm localidad = (LocalidadEdm) entity.get();
+
+    		Integer localidadID = localidad.getLocalidadId();
+    		Integer provinciaID = localidad.getProvinciaId();
+    		Integer paisID = localidad.getPaisId();
+
+    		try {
+    			localidadService.borrar(paisID, provinciaID, localidadID);
+				return;
+			} catch (Exception e) {
+				String message = SQLExceptionParser.parse(e);
+				throw new ODataDataSourceException(message);
+			}
+        }
+    	
+    	throw new ODataDataSourceException("LOS DATOS NO CORRESPONDEN A LA ENTIDAD LOCALIDAD");
+	}
+
+	@Override
+	public void createLink(ODataUri uri, ODataLink link, EntityDataModel entityDataModel) throws ODataException {
+		// NO HACER NADA
+	}
+
+	@Override
+	public void deleteLink(ODataUri uri, ODataLink link, EntityDataModel entityDataModel) throws ODataException {
+		// NO HACER NADA
+	}
+
+	@Override
+	public TransactionalDataSource startTransaction() {
+		throw new ODataSystemException("No support for transactions");
+	}
+
+	@Override
+	public boolean isSuitableFor(ODataRequestContext requestContext, String entityType) throws ODataDataSourceException {
+		return requestContext.getEntityDataModel().getType(entityType).getJavaType().equals(LocalidadEdm.class);
+	}
+
+	@Override
+	public DataSource getDataSource(ODataRequestContext requestContext) {
+		return this;
+	}
+
+	@Override
+	public QueryOperationStrategy getStrategy(ODataRequestContext requestContext, QueryOperation operation, TargetType expectedODataEntityType) throws ODataException {
+
+		LocalidadesStrategyBuilder builder = new LocalidadesStrategyBuilder();
+		BooleanExpression expression = builder.buildCriteria(operation, requestContext);
+		List<Sort.Order> orderByList = builder.getOrderByList();
+		
+		int limit = builder.getLimit();
+        int skip = builder.getSkip();
+		List<String> propertyNames = builder.getPropertyNames();
+		
+		Page<LocalidadEntity> pageLocalidadEntity = localidadService.ejecutarConsulta(expression, orderByList, limit);
+		
+		List<LocalidadEntity> localidadEntities = pageLocalidadEntity.getContent();
+		
+		return () -> {
+
+			List<LocalidadEdm> filtered = localidadEntities.stream().map(entity -> { return new LocalidadEdm(entity); }).collect(Collectors.toList());
+			
+			long count = 0;
+        	
+            if (builder.isCount() || builder.includeCount()) {
+                count = filtered.size();
+
+                if (builder.isCount()) {
+                    return QueryResult.from(count);
+                }
+            }
+
+            if (skip != 0 || limit != Integer.MAX_VALUE) {
+                filtered = filtered.stream().skip(skip).collect(Collectors.toList());
+            }
+
+            if (propertyNames != null && !propertyNames.isEmpty()) {
+                try {
+                    return QueryResult.from(EdmUtil.getEdmPropertyValue(filtered.get(0), propertyNames.get(0)));
+                } catch (IllegalAccessException e) {
+                    return QueryResult.from(Collections.emptyList());
+                }
+            }
+            
+            QueryResult result = QueryResult.from(filtered);
+            if (builder.includeCount()) {
+                result = result.withCount(count);
+            }
+            return result;
+		};
+	}
+}
